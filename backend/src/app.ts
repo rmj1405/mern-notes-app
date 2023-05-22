0ec2b51c7bd29d0d5f7f641420b488980f147e98
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-05-22 12:54:36 +0800

    Create mongodb models and server error handling for express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,40 @@
 import "dotenv/config"
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
+import NoteModel from "./models/note"
 
 //call express which is our server where we create the endpoints
 const app = express()
 
 
-//get request
-app.get("/", (req, res) => {
-    res.send("hello,world")
+//get request endpoint
+app.get("/", async (req, res, next) => { //await can only be used inside async
+    try {
+        // throw Error("Rubiryo error!") //throws an error
+        const notes = await NoteModel.find().exec() //await is syntactic sugar for promises, it waits until promise returns
+        res.status(200).json(notes)
+    } catch (error) {
+        //express middleware
+        next(error)
+    }
+
+})
+
+//create a middleware to catch requests that dont have a route set up for it
+app.use((req, res, next) => {
+    next(Error("Endpoint not found!"))
+})
+
+
+//express error handler: has to take these exact arguments with these types
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res:Response, next:NextFunction ) => {
+        //log the error
+        console.error(error)
+        let errorMessage = "An unknown error occured"
+        //check if it is actually of type Error
+        if (error instanceof Error) errorMessage = error.message
+        res.status(500).json({ error: errorMessage })
 })
 
 export default app
---
+++ backend/src/models/note.ts
@@ -0,0 +1,13 @@
+import { InferSchemaType, model, Schema } from "mongoose"
+
+//scheme for notes
+const noteSchema = new Schema({
+    title: { type: String, required: true },
+    text: { type: String },
+}, { timestamps: true }) //this is to be managed by mongodb
+
+//create a type of Note for typesafety in Typescript
+type Note = InferSchemaType<typeof noteSchema>
+
+//create a model of type Note to be exported for use in code
+export default model<Note>("Note", noteSchema)
