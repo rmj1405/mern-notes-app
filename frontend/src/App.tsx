1dada944b1c0637cf31a003b2a4b9c31ad2c8e04
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-05-23 22:30:40 +0800

    Connect backend with frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
+import { Note } from './models/note';
 
 function App() {
   //create a state
-  const [clickCount, setClickCount] = useState(0)
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  //execute side effects outside of the  rendering of the component itself
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch("/api/notes", { method: "GET" });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        //open up popup window in browser
+        alert(error);
+      }
+
+    }
+    loadNotes();
+    //empty array means that useEffect only executes once at the beginning
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code>
-        </p>
-        <Button onClick={() => setClickCount(clickCount + 1)}>
-          Clicked {clickCount} times
-        </Button>
-      </header>
+      {JSON.stringify(notes)}
     </div>
   );
 }
